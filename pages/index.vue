2eb10f7529a3f41604fe38be35e188bf02c44d38
winterguard <aliasbonfire@gmail.com>
2020-10-26 09:21:48 +0900

    deep selector

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,8 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">Study-NuxtJS-Basic</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="parent">
+      parent
+      <div class="child">child</div>
     </div>
   </div>
 </template>
@@ -32,43 +14,13 @@ export default Vue.extend({})
 </script>
 
 <style lang="scss" scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  .NuxtLogo {
-    animation: 3s appear;
+.parent {
+  color: orangered;
+}
+
+.child {
+  .parent & ::v-deep {
+    color: blueviolet;
   }
-}
-
-.NuxtLogo {
-  .container & ::v-deep {
-    color: red;
-  }
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
